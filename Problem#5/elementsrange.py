0656be11a064bd0433b2c36f18f5aaae32b83c90
TKDFlash <113069942+TKDFlash@users.noreply.github.com>
2024-03-06 21:33:44 -0600

    com

--- Problem#5/elementsrange.py
+++ Problem#5/elementsrange.py
@@ -5,15 +5,14 @@ Thursday @ 2pm
 
 def filter_and_print_range(input_list, min_val, max_val):
     #write your code here
-    integer_list = list(map(int, input_list.split()))
 
-    for num in integer_list:
+    for num in input_list:
         if min_val <= num <= max_val:
             print(num, end=",")
 
 if __name__ == '__main__':
     # Get input for the list of integers
-    user_input_list = input("Enter a space-separated string of numbers: ")
+    user_input_list = list(map(int, input("Enter a space-separated string of numbers: ").split()))
 
     # Get input for the range (min and max values)
     min_val, max_val = map(int, input("Enter the min and max values separated by a space: ").split())
